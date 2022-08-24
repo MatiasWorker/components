ec501221d87e51a812f625b602b68033977341a4
Matias <matias.trujillo@2brains.cl>
2022-08-24 14:51:10 -0400

    end button component

--- packages/Button/src/index.stories.tsx
+++ packages/Button/src/index.stories.tsx
@@ -1,3 +1,4 @@
+import "@bxreact/theme";
 import { Button } from "./";
 
 export default {
@@ -8,3 +9,50 @@ export default {
 };
 
 export const Default = () => <Button>Button</Button>;
+
+export const DefaultWithIcon = () => (
+    <Button>
+        <span>★</span>
+        <span>Button</span>
+    </Button>
+);
+
+export const DefaultXs = () => <Button size="xs">Button</Button>;
+
+export const DefaultMd = () => <Button size="md">Button</Button>;
+
+export const DefaultLg = () => <Button size="lg">Button</Button>;
+
+export const DefaulttDisabled = () => <Button disabled>Button</Button>;
+
+export const Thead = () => <Button thead>Button</Button>;
+
+export const TheadInfo = () => (
+    <Button thead status="info">
+        Button
+    </Button>
+);
+
+export const TheadSuccess = () => (
+    <Button thead status="success">
+        Button
+    </Button>
+);
+
+export const TheadWarning = () => (
+    <Button thead status="warning">
+        Button
+    </Button>
+);
+
+export const TheadDanger = () => (
+    <Button thead status="danger">
+        Button
+    </Button>
+);
+
+export const TheadDisabled = () => (
+    <Button thead disabled>
+        Button
+    </Button>
+);
--- packages/Button/src/index.tsx
+++ packages/Button/src/index.tsx
@@ -2,23 +2,38 @@ import { MouseEventHandler } from "react";
 import "./index.css";
 
 interface Props {
-    href: string;
-    open: boolean;
+    size?: "xs" | "sm" | "md" | "lg";
+    href?: string;
+    open?: boolean;
     disabled?: boolean;
+    thead?: boolean;
+    status?: "info" | "success" | "warning" | "danger";
     onClick?: (event: MouseEventHandler<HTMLButtonElement>) => void;
     children: any;
 }
 
-export function FieldSwitch({
+export function Button({
     onClick,
     disabled,
     href,
     open,
     children,
+    size,
+    thead,
+    status,
 }: Props): JSX.Element {
     const Type = href ? "a" : "button";
+    const className: string[] = ["button"];
+
+    if (size) className.push(`button--${size}`);
+
+    if (thead) className.push(`button--thead`);
+
+    if (status) className.push(`button--status-${status}`);
+
     return (
         <Type
+            className={className.join(" ")}
             onClick={onClick as any}
             href={href}
             target={open ? "_blank" : null}
