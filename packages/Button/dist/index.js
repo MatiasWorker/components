1251f672aca32b83fc9a9e9e9f2d10303fae31dd
Matias <matias.trujillo@2brains.cl>
2022-08-24 15:45:25 -0400

    Update

--- packages/Button/dist/index.js
+++ packages/Button/dist/index.js
@@ -3,113 +3,131 @@ import { createElement as _jsx } from "react";
 
 // src/index.css
 import css from "ustyler";
-var src_default = css`.field-select {
-    --select-background: var(--bx-select-background, #eaf8ff);
-    --select-border-radius: var(--bx-lect-border-radius, 5px);
-    --select-space-x: var(--bx-lect-space-x, 8px);
-    --select-font-weight: var(--bx-lect-font-weight, 700);
-    --select-font-size: var(--bx-lect-font-size, 12px);
-    --select-icon-width: var(--bx-lect-icon-width, 15px);
-    --select-icon-color: var(--bx-select-icon-color, #ff7a00);
-    --select-width: 100%;
-    position: relative;
-    font-weight: var(--select-font-weight);
-    font-size: var(--select-font-size);
-}
+var src_default = css`.button {
+    --button-padding: var(--bx-input-sm-padding);
+    --button-height: var(--bx-input-sm-height);
+    --button-radius: var(--bx-input-sm-radius);
+    --button-bgcolor: var(--bx-button-filled-bgcolor);
+    --button-color: var(--bx-button-filled-color);
+    --button-font-size: var(--bx-input-sm-font-size);
 
-.field-select--cell {
-    min-width: 100%;
-    height: 100%;
-    --select-width: auto;
-    --select-border-radius: 0px;
-    --select-font-weight: unset;
-    --select-font-size: unset;
-    --select-background: transparent;
-}
-
-.field-select--danger {
-    --select-background: var(--bx-field-select-warning, #fe9292);
-}
-
-.field-text--success {
-    --select-background: var(--bx-field-select-success, transparent);
-}
-
-.field-text--warning {
-    --select-background: var(--bx-field-select-warning, transparent);
-}
-
-.field-select_select {
-    width: var(--select-width);
-    min-height: 100%;
-    min-width: 100%;
-    margin: 0;
-    background: var(--select-background);
-    border-radius: var(--select-border-radius);
-    border: none;
-    font-family: unset;
-    font-weight: unset;
-    font-size: unset;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: var(--button-padding);
+    min-height: var(--button-height);
     box-sizing: border-box;
-    padding: 0px var(--select-space-x);
+    flex-flow: row nowrap;
+    border-radius: var(--button-radius);
+    font-family: var(--bx-input-font-family);
+    font-weight: var(--bx-input-font-weight);
+    border: none;
+    background: var(--button-bgcolor);
+    color: var(--button-color);
+    font-size: var(--button-font-size);
+    line-height: 1;
+    cursor: pointer;
+    text-decoration: none;
+    gap: var(--bx-button-gap);
 }
 
-.field-select_select option {
-    background: white;
-    color: black;
+.button--xs {
+    --button-padding: var(--bx-input-xs-padding);
+    --button-height: var(--bx-input-xs-height);
+    --button-radius: var(--bx-input-xs-radius);
+    --button-font-size: var(--bx-input-xs-font-size);
 }
 
-.field-select--pagination .field-select_select {
-    appearance: none;
-    padding: calc(var(--select-space-x) / 2)
-        calc(var(--select-space-x) * 2 + var(--select-icon-width))
-        calc(var(--select-space-x) / 2) var(--select-space-x);
+.button--md {
+    --button-padding: var(--bx-input-md-padding);
+    --button-height: var(--bx-input-md-height);
+    --button-radius: var(--bx-input-md-radius);
+    --button-font-size: var(--bx-input-md-font-size);
 }
 
-.field-select_icon {
-    position: absolute;
-    width: var(--select-icon-width);
-    top: 50%;
-    right: var(--select-space-x);
-    transform: translateY(-50%);
+.button--lg {
+    --button-padding: var(--bx-input-lg-padding);
+    --button-height: var(--bx-input-lg-height);
+    --button-radius: var(--bx-input-lg-radius);
+    --button-font-size: var(--bx-input-lg-font-size);
+}
+
+.button:disabled,
+.button[disabled] {
+    pointer-events: none;
+    --button-bgcolor: var(--bx-button-filled-disabled-bgcolor);
+    --button-color: var(--bx-button-filled-disabled-color);
+}
+
+.button--thead {
+    --button-bgcolor: var(--bx-button-thead-bgcolor);
+    --button-color: var(--bx-button-thead-color);
+    box-shadow: var(--bx-button-shadow);
+}
+
+.button--thead.button--status-info {
+    --button-bgcolor: var(--bx-button-thead-info-bgcolor);
+    --button-color: var(--bx-button-thead-info-color);
+}
+
+.button--thead.button--status-success {
+    --button-bgcolor: var(--bx-button-thead-success-bgcolor);
+    --button-color: var(--bx-button-thead-success-color);
+}
+
+.button--thead.button--status-warning {
+    --button-bgcolor: var(--bx-button-thead-warning-bgcolor);
+    --button-color: var(--bx-button-thead-warning-color);
+}
+
+.button--thead.button--status-danger {
+    --button-bgcolor: var(--bx-button-thead-danger-bgcolor);
+    --button-color: var(--bx-button-thead-danger-color);
+}
+
+.button--thead:disabled,
+.button--thead[disabled] {
+    --button-bgcolor: var(--bx-button-thead-disabled-bgcolor);
+    --button-color: var(--bx-button-thead-disabled-color);
+}
+
+.button--icon {
+    width: var(--button-height);
+    height: var(--button-height);
+    padding: 0px;
 }
 `;
 
 // src/index.tsx
-function FieldSelect({
-  value,
-  placeholder,
-  options = [],
-  onChange,
-  appearance,
-  status
+function Button({
+  onClick,
+  disabled,
+  href,
+  open,
+  children,
+  size,
+  thead,
+  status,
+  icon
 }) {
-  return /* @__PURE__ */ _jsx("div", {
-    className: `field-select field-select--${appearance} field-select--${status}`
-  }, appearance === "pagination" && /* @__PURE__ */ _jsx("svg", {
-    className: "field-select_icon",
-    width: "15",
-    height: "9",
-    viewBox: "0 0 15 9",
-    fill: "none"
-  }, /* @__PURE__ */ _jsx("path", {
-    d: "M14.3215 0.97836C14.0343 0.696178 13.5809 0.696178 13.2937 0.97836L7.64797 6.52545L1.75287 0.970934C1.46567 0.696178 1.00464 0.703604 0.732562 0.993211C0.46048 1.28282 0.46048 1.72837 0.755235 1.9957L7.15671 8.03289C7.30031 8.16656 7.48169 8.23339 7.66308 8.23339C7.85203 8.23339 8.03341 8.16656 8.17701 8.02547L14.3215 1.98827C14.6012 1.70609 14.6012 1.26054 14.3215 0.97836Z",
-    fill: "var(--select-icon-color)"
-  })), /* @__PURE__ */ _jsx("select", {
-    value: value || "",
-    className: "field-select_select",
-    onChange: (event) => {
-      onChange && onChange(event.currentTarget.value);
-    }
-  }, placeholder && /* @__PURE__ */ _jsx("option", {
-    value: "",
-    disabled: true,
-    key: "placeholder"
-  }, placeholder), options.map((option) => /* @__PURE__ */ _jsx("option", {
-    key: option.value,
-    value: option.value
-  }, option.label))));
+  const Type = href ? "a" : "button";
+  const className = ["button"];
+  if (size)
+    className.push(`button--${size}`);
+  if (thead)
+    className.push(`button--thead`);
+  if (status)
+    className.push(`button--status-${status}`);
+  if (icon)
+    className.push(`button--icon`);
+  return /* @__PURE__ */ _jsx(Type, {
+    className: className.join(" "),
+    onClick,
+    href,
+    target: open ? "_blank" : null,
+    disabled
+  }, children);
 }
 export {
-  FieldSelect
+  Button
 };
--- packages/Button/types/index.d.ts
+++ packages/Button/types/index.d.ts
@@ -1,14 +1,15 @@
+import { MouseEventHandler } from "react";
 import "./index.css";
 interface Props {
-    value: string;
-    placeholder?: string;
-    onChange?: (value: string) => void;
-    options: {
-        value: string;
-        label?: string;
-    }[];
-    appearance?: "pagination" | "cell";
-    status?: "success" | "warning" | "danger" | "";
+    size?: "xs" | "sm" | "md" | "lg";
+    href?: string;
+    open?: boolean;
+    disabled?: boolean;
+    thead?: boolean;
+    icon?: boolean;
+    status?: "info" | "success" | "warning" | "danger";
+    onClick?: (event: MouseEventHandler<HTMLButtonElement>) => void;
+    children: any;
 }
-export declare function FieldSelect({ value, placeholder, options, onChange, appearance, status, }: Props): JSX.Element;
+export declare function Button({ onClick, disabled, href, open, children, size, thead, status, icon, }: Props): JSX.Element;
 export {};
