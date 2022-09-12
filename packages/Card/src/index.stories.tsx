44a4edd3305461c6db31fa34cc9fa71df1166480
Matias <matias.trujillo@2brains.cl>
2022-09-12 11:58:02 -0300

    update workspace

--- packages/Card/src/index.stories.tsx
+++ packages/Card/src/index.stories.tsx
@@ -11,7 +11,15 @@ export default {
     },
 };
 
-export const Default = (props) => <Card {...props}>welcome!</Card>;
+export const Default = (props) => (
+    <Card {...props}>
+        culpa ea laborum sint esse sit eu esse voluptate commodo nostrud et
+        Lorem id excepteur cillum in sint nisi esse Lorem et esse Lorem culpa
+        veniam anim culpa non exercitation cupidatat ex nostrud aute elit veniam
+        officia proident laboris exercitation labore fugiat tempor cupidatat in
+        commodo et aute et aliqua consectetur
+    </Card>
+);
 
 Default.args = {
     size: "",
