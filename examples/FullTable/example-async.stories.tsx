853266c91d0a6845670894f1b7e71d5bdc1e5f44
Matias <matias.trujillo@2brains.cl>
2022-11-09 15:43:17 -0300

    example-async is added

---
+++ examples/FullTable/example-async.stories.tsx
@@ -0,0 +1,97 @@
+import { useState, useEffect } from "react";
+import { Table } from "@bxreact/table";
+import { Pagination } from "@bxreact/pagination";
+
+export default {
+    title: "Table/Example with request",
+    argTypes: {
+        backgroundColor: { control: "color" },
+    },
+};
+
+interface RootObject {
+    avatar: string;
+    bio: string;
+    email: string;
+    emailVerified: boolean | string;
+    firstName: string;
+    fullName: string;
+    id: number;
+    lastName: string;
+    newKey: number;
+    phone: string;
+    selected: boolean;
+    twitterHandle: string;
+    username: string;
+}
+
+export function Example() {
+    const [currentPage, setCurrentPage] = useState(0);
+    const [limit, setLimit] = useState(10);
+    const [data, setData] = useState<{ items: RootObject[]; size: number }>({
+        items: [],
+        size: 0,
+    });
+
+    useEffect(() => {
+        fetch(
+            `https://example-data.draftbit.com/users?_limit=10&_page=${currentPage}`
+        )
+            .then((res) => res.json())
+            .then((items) => setData({ items, size: 500 }));
+    }, [currentPage]);
+
+    return (
+        <div>
+            <Table
+                rowStyle={(data) =>
+                    data.id === 1
+                        ? {
+                              "--table-row-background": "#FFE9E9",
+                          }
+                        : null
+                }
+                data={data.items}
+                header={{
+                    id: "",
+                    fullName: "Nombre",
+                    avatar: "Imagen",
+                    newKey: "newKey",
+                    email: "Email",
+                    emailVerified: "Seguro",
+                    phone: "Telefono",
+                    bio: "Bio",
+                }}
+            ></Table>
+            <Pagination
+                value={limit}
+                pagesPerPage={[10, 20, 30]}
+                pagedLabel="Filas por página"
+                moveLabel={`${currentPage + 1} de ${data.size}`}
+                onChangePagesPerPage={(value) => {
+                    setLimit(Number(value));
+                }}
+                onChangeMove={(type) => {
+                    switch (type) {
+                        case "<":
+                            if (currentPage - 1 > 0) {
+                                setCurrentPage(currentPage - 1);
+                            }
+                            break;
+                        case ">":
+                            if (currentPage + 1 < data.size) {
+                                setCurrentPage(currentPage + 1);
+                            }
+                            break;
+                        case "<<":
+                        case ">>":
+                            return false;
+                    }
+                }}
+                isMoveDisabled={(type) =>
+                    type == ">>" || type == "<<" ? true : false
+                }
+            />
+        </div>
+    );
+}
