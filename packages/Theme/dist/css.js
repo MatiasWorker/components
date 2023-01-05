ebac4fe06c9877581900a8df258e5623f806bd74
Matias <matias.trujillo@2brains.cl>
2023-01-05 15:26:12 -0300

    Tokens

--- packages/Theme/dist/css.js
+++
@@ -1,6 +0,0 @@
-// src/css.ts
-import css from "@bxreact/css";
-var css_default = css;
-export {
-  css_default as default
-};
--- packages/Theme/types/index.d.ts
+++
@@ -1,7 +0,0 @@
-import "./generic.css";
-import "./font.css";
-import "./default.css";
-import "./colors.css";
-import "./input.css";
-import "./button.css";
-import "./card.css";
