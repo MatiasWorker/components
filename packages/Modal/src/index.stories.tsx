b897d2a0ca1b7f1f87d1d829239b88deeaf549ff
Matias <matias.trujillo@2brains.cl>
2023-01-10 15:28:28 -0300

    example

--- packages/Modal/src/index.stories.tsx
+++ packages/Modal/src/index.stories.tsx
@@ -1,24 +1,29 @@
 import "@bxreact/theme";
 import { Modal, ModalHeader } from "./";
 import { Card } from "@bxreact/card";
+import { useState } from "react";
 
 export default {
     title: "Generic/Modal",
     args: {
-        show: true,
+        show: false,
         zIndex: 10,
     },
 };
 
-export const Default = (props) => (
-    <Modal {...props} onClosed={() => console.log("closed")}>
-        <Card padding="sm">
-            <ModalHeader>Modal</ModalHeader>
-            culpa ea laborum sint esse sit eu esse voluptate commodo nostrud et
-            Lorem id excepteur cillum in sint nisi esse Lorem et esse Lorem
-            culpa veniam anim culpa non exercitation cupidatat ex nostrud aute
-            elit veniam officia proident laboris exercitation labore fugiat
-            tempor cupidatat in commodo et aute et aliqua consectetur
-        </Card>
-    </Modal>
-);
+export const Default = (props) => {
+    const [show, setShow] = useState(props.show);
+    return (
+        <Modal {...props} onClosed={() => setShow(false)} show={show}>
+            <Card padding="sm">
+                <ModalHeader>Modal</ModalHeader>
+                culpa ea laborum sint esse sit eu esse voluptate commodo nostrud
+                et Lorem id excepteur cillum in sint nisi esse Lorem et esse
+                Lorem culpa veniam anim culpa non exercitation cupidatat ex
+                nostrud aute elit veniam officia proident laboris exercitation
+                labore fugiat tempor cupidatat in commodo et aute et aliqua
+                consectetur
+            </Card>
+        </Modal>
+    );
+};
