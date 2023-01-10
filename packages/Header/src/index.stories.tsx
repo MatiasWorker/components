4f17e265895a3e6fdf5efb3e6beda712122d5296
Matias <matias.trujillo@2brains.cl>
2023-01-10 15:00:47 -0300

    small is added

--- packages/Header/src/index.stories.tsx
+++ packages/Header/src/index.stories.tsx
@@ -3,12 +3,20 @@ import { Header } from "./";
 export default {
     title: "Generic/Header",
     args: {
+        title: "1. ¿Qué estás enviando?",
+        small: false,
         required: true,
     },
 };
 
 export const Default = (props) => (
-    <Header title="3. ¿Qué estás enviando?" required={!!props.required}>
+    <Header {...props}>
+        Ingresa las dimensiones de cada pieza de tu envío, su valor y categoría
+    </Header>
+);
+
+export const WithRightIcon = (props) => (
+    <Header {...props} rightIcon="😀">
         Ingresa las dimensiones de cada pieza de tu envío, su valor y categoría
     </Header>
 );
--- packages/Header/src/index.tsx
+++ packages/Header/src/index.tsx
@@ -6,18 +6,33 @@ export function Header({
     title,
     children,
     required,
+    small,
+    rightIcon,
+    leftIcon,
 }: {
     title: ReactNode;
     required?: boolean;
     children?: ReactNode;
+    small?: boolean;
+    leftIcon?: ReactNode;
+    rightIcon?: ReactNode;
 }) {
+    const Size = small ? "h3" : "h2";
     return (
         <header className="bx-header">
             <div className="bx-header_title">
-                <h2>
+                {leftIcon && (
+                    <div className="bx-header_title_left-icon">{leftIcon}</div>
+                )}
+                <Size>
                     {title}
                     {required && <span className="bx-header_required">*</span>}
-                </h2>
+                </Size>
+                {rightIcon && (
+                    <div className="bx-header_title_right-icon">
+                        {rightIcon}
+                    </div>
+                )}
             </div>
             {children && <div className="bx-header_content">{children}</div>}
         </header>
