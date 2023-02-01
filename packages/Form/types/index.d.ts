cfb359c5a21b5efe444d673d68cd109fa1149091
Matias <matias.trujillo@2brains.cl>
2023-02-01 13:59:03 -0300

    types

--- packages/Form/types/index.d.ts
+++ packages/Form/types/index.d.ts
@@ -1,8 +1,8 @@
 import "./index.css";
-import { InputData, InputForm, InputReplace } from "./types";
+import { InputData, InputForm, InputReplace, InputMetaData } from "./types";
 export { InputTypes, InputForm, InputUnknown } from "./types";
 export { logic, required } from "./utils";
-export interface Props<FormData extends InputData, MetaData extends InputData> {
+export interface Props<FormData extends InputData, MetaData extends InputMetaData> {
     formData: FormData;
     metaData?: MetaData;
     form: InputForm<FormData, MetaData>;
@@ -10,4 +10,4 @@ export interface Props<FormData extends InputData, MetaData extends InputData> {
     onChange: (data: FormData) => void;
     types?: InputReplace<FormData, MetaData>;
 }
-export declare function Form<FormData extends InputData, MetaFormData extends InputData>({ formData, metaData, form, onChange, columns, types, }: Props<FormData, MetaFormData>): JSX.Element;
+export declare function Form<FormData extends InputData, MetaFormData extends InputMetaData>({ formData, metaData, form, onChange, columns, types, }: Props<FormData, MetaFormData>): JSX.Element;
--- packages/Form/types/types.d.ts
+++ packages/Form/types/types.d.ts
@@ -2,6 +2,9 @@ import type { Status } from "@bxreact/theme";
 export interface InputData {
     [prop: string]: number | boolean | string | File;
 }
+export interface InputMetaData {
+    [prop: string]: any;
+}
 export declare type InputOptions<Value extends unknown> = {
     label: string;
     value?: Value;
@@ -14,7 +17,7 @@ export interface InputLogic<Data extends InputData> {
     required?: boolean | Logic<Data>;
     logic?: Logic<Data> | Logic<Data>[];
 }
-export interface InputGeneric<Data extends InputData, MetaData extends InputData, Value extends unknown> extends InputLogic<Data & MetaData> {
+export interface InputGeneric<Data extends InputData, MetaData extends InputMetaData, Value extends unknown> extends InputLogic<Data & MetaData> {
     label?: string;
     detail?: string;
     description?: string;
@@ -26,52 +29,52 @@ export interface InputGeneric<Data extends InputData, MetaData extends InputData
     placeholder?: string;
     status?: Status;
 }
-export interface InputSelect<Data extends InputData, MetaData extends InputData, Value extends unknown> extends InputGeneric<Data, MetaData, Value> {
+export interface InputSelect<Data extends InputData, MetaData extends InputMetaData, Value extends unknown> extends InputGeneric<Data, MetaData, Value> {
     type: "select";
     options: InputOptions<Value>[];
 }
-export interface InputText<Data extends InputData, MetaData extends InputData, Value extends unknown> extends InputGeneric<Data, MetaData, Value> {
+export interface InputText<Data extends InputData, MetaData extends InputMetaData, Value extends unknown> extends InputGeneric<Data, MetaData, Value> {
     type: "text";
     minLength?: number;
     maxLength?: number;
 }
-export interface InputDate<Data extends InputData, MetaData extends InputData, Value extends unknown> extends InputGeneric<Data, MetaData, Value> {
+export interface InputDate<Data extends InputData, MetaData extends InputMetaData, Value extends unknown> extends InputGeneric<Data, MetaData, Value> {
     type: "date";
     min?: number;
     max?: number;
 }
-export interface InputEmail<Data extends InputData, MetaData extends InputData, Value extends unknown> extends InputGeneric<Data, MetaData, Value> {
+export interface InputEmail<Data extends InputData, MetaData extends InputMetaData, Value extends unknown> extends InputGeneric<Data, MetaData, Value> {
     type: "email";
 }
-export interface InputTextArea<Data extends InputData, MetaData extends InputData, Value extends unknown> extends InputGeneric<Data, MetaData, Value> {
+export interface InputTextArea<Data extends InputData, MetaData extends InputMetaData, Value extends unknown> extends InputGeneric<Data, MetaData, Value> {
     type: "textarea";
 }
-export interface InputNumber<Data extends InputData, MetaData extends InputData, Value extends unknown> extends InputGeneric<Data, MetaData, Value> {
+export interface InputNumber<Data extends InputData, MetaData extends InputMetaData, Value extends unknown> extends InputGeneric<Data, MetaData, Value> {
     type: "number";
     min?: number;
     max?: number;
     step?: number;
 }
-export interface InputCheckbox<Data extends InputData, MetaData extends InputData, Value extends unknown> extends InputGeneric<Data, MetaData, Value> {
+export interface InputCheckbox<Data extends InputData, MetaData extends InputMetaData, Value extends unknown> extends InputGeneric<Data, MetaData, Value> {
     type: "checkbox" | "switch";
     checked?: boolean;
 }
-export interface InputRadio<Data extends InputData, MetaData extends InputData, Value extends unknown> extends InputGeneric<Data, MetaData, Value> {
+export interface InputRadio<Data extends InputData, MetaData extends InputMetaData, Value extends unknown> extends InputGeneric<Data, MetaData, Value> {
     type: "radio";
     options: InputOptions<Value>;
 }
-export interface InputFile<Data extends InputData, MetaData extends InputData, Value extends unknown> extends InputGeneric<Data, MetaData, Value> {
+export interface InputFile<Data extends InputData, MetaData extends InputMetaData, Value extends unknown> extends InputGeneric<Data, MetaData, Value> {
     type: "file";
 }
-export interface InputCustom<Data extends InputData, MetaData extends InputData, Value extends unknown> extends InputGeneric<Data, MetaData, Value> {
+export interface InputCustom<Data extends InputData, MetaData extends InputMetaData, Value extends unknown> extends InputGeneric<Data, MetaData, Value> {
     type: "custom";
     render?: (data: Data & MetaData, value: Value) => any;
 }
-export declare type InputTypes<Data extends InputData, MetaData extends InputData, Value> = InputSelect<Data, MetaData, Value> | InputRadio<Data, MetaData, Value> | InputText<Data, MetaData, Value> | InputNumber<Data, MetaData, Value> | InputCheckbox<Data, MetaData, Value> | InputDate<Data, MetaData, Value> | InputFile<Data, MetaData, Value> | InputEmail<Data, MetaData, Value> | InputCustom<Data, MetaData, Value> | InputTextArea<Data, MetaData, Value>;
-export declare type InputForm<Data extends InputData, MetaData extends InputData> = {
+export declare type InputTypes<Data extends InputData, MetaData extends InputMetaData, Value> = InputSelect<Data, MetaData, Value> | InputRadio<Data, MetaData, Value> | InputText<Data, MetaData, Value> | InputNumber<Data, MetaData, Value> | InputCheckbox<Data, MetaData, Value> | InputDate<Data, MetaData, Value> | InputFile<Data, MetaData, Value> | InputEmail<Data, MetaData, Value> | InputCustom<Data, MetaData, Value> | InputTextArea<Data, MetaData, Value>;
+export declare type InputForm<Data extends InputData, MetaData extends InputMetaData> = {
     [Prop in keyof Data]: InputTypes<Data, MetaData, Data[Prop]> | InputTypes<Data, MetaData, Data[Prop]>[];
 };
-export declare type InputReplace<Data extends InputData, MetaData extends InputData> = {
+export declare type InputReplace<Data extends InputData, MetaData extends InputMetaData> = {
     [Prop in keyof Data]?: (input: InputUnknown, data: Data & MetaData, value: Data[Prop]) => InputUnknown | {
         [input: string]: InputUnknown;
     };
