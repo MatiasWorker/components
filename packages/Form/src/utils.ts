ede9538ac7afaaf621dff5b4bcc12839b058ec2f
Matias <matias.trujillo@2brains.cl>
2023-02-16 11:55:36 -0300

    Fix trim

--- packages/Form/src/utils.ts
+++ packages/Form/src/utils.ts
@@ -27,7 +27,7 @@ export const required = (form: Form, data: { [prop: string]: any }) =>
                     input.type === "textarea" ||
                     typeof data[prop] === "string"
                 ) {
-                    return !data[prop].trim();
+                    return !data[prop]?.trim();
                 } else {
                     return data[prop] == null;
                 }
